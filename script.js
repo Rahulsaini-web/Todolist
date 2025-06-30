dd69229888a2a3b2e025f89d6f3ef13a7483f6c1
rahulsaini-web <rrahulsaini9351@gmail.com>
2025-06-30 14:31:07 +0100

    first commit

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function AddTask(){
             button.innerHTML = "Delete";
             li.appendChild(button);
     }
+    alert("Your task has been added successfully!");
     inputBox.value = "";
     saveData();
 }
@@ -21,10 +22,12 @@ function AddTask(){
 listContainer.addEventListener("click" , function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+       alert("Your task has been checked successfully!");
         saveData();
     }
     else if(e.target.tagName === "BUTTON"){
         e.target.parentElement.remove();
+       alert("Task deleted successfully."); 
         saveData();
     }
 },false);
